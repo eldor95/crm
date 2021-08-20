06497c535430708715b1aa5da2e51521c22d6555
eldor <eldorodilov2@gmail.com>
2021-08-20 07:53:03 +0500

    add exten

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+const session = require("express-session");
 const mongoose = require('mongoose')
+const cookieParser = require("cookie-parser");
+// const cors = require('cors')
 const MongoURI = 'mongodb://localhost:27017/crm';
+const MongoDBSession = require("connect-mongodb-session")(session);
 
 app.use(express.json())
 app.use(express.urlencoded({
@@ -16,7 +20,31 @@ mongoose.connect(MongoURI, {
 }).then(() => {
     console.log('MongoDb connected! ');
 });
+// app.use(cors());
 
+app.locals.moment = require("moment");
+
+const store = new MongoDBSession({
+    uri: MongoURI,
+    collection: "MYSession",
+});
+
+app.use(cookieParser());
+
+app.use(
+    session({
+        secret: "odiloveldor",
+        saveUninitialized: false,
+        store: store,
+        resave: false,
+
+        cookie: {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 2,
+            sameSite: "strict",
+        },
+    })
+);
 
 
 
